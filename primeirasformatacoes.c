89c752886cf7dedf8477f2f79aec896d2808e70f
Lucas Claro <lucas-claro@hotmail.com>
2022-08-05 08:48:06 -0300

    Create primeirasformatacoes.c

---
+++ primeirasformatacoes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	printf("Oi, tudo bem: Tenho 6 anos e programo\n");
+	
+	printf("Valor inteiro: %d.\n", 10);
+	
+	printf("Valor real: %f.\n", 3.141516);
+	
+	printf("Valor real com apenas duas casas: %.2f.\n", 3.141516);
+	
+	printf("Dado de texto: %c.\n", 'a');
+	
+	printf("Dado de texto: %s.\n", "testando");
+	
+}
