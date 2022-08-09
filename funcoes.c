4731e7584de7b277d331efdf48b8c590459313a1
Lucas Claro <lucas-claro@hotmail.com>
2022-08-09 08:03:21 -0300

    Create funcoes.c

---
+++ funcoes.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int somar() {
+	
+	return 2 + 3;
+	
+}
+
+
+int main(int argc, char *argv[]) {
+	
+	int resultado = 0;
+	resultado = somar();
+	printf("O resultado da funcao e = %d", resultado);
+	
+	
+	
+	return 0;
+}
